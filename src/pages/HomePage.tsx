8a4a0036bada0554914b19f9fff450ed60df626c
Trlaic <igortrlajic@gmail.com>
2024-02-19 23:42:42 +0100

    x03: Prepare HomePage for three.js element

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,15 @@
+import styles from './HomePage.module.scss'
+
 const HomePage = () => {
     return (
-        <h1>HomePage</h1>
+        <div className={styles.container}>
+            <h1 className={styles.heading}>Portfolio | Igor Trlaic</h1>
+            <div className={styles.button_container}>
+                <button>Standard</button>
+                <canvas className={styles.webgl}></canvas>
+                <button>Interesting</button>
+            </div>
+        </div>
     )
 }
 
