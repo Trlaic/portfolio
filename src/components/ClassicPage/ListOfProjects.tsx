fe72e3a435d8f59d1b374b7f65a38bacbde95911
Trlaic <igortrlajic@gmail.com>
2024-02-23 17:45:03 +0100

    x09: Work on ListOfProjects component

--- src/components/ClassicPage/ListOfProjects.tsx
+++ src/components/ClassicPage/ListOfProjects.tsx
@@ -2,7 +2,26 @@ import style from './ListOfProjects.module.scss'
 
 const ListOfProjects = () => {
     return (
-        <div className={style.container}>List of Projects</div>
+        <div className={style.container}>
+            <h2 className={style.heading}>Projects</h2>
+            <ul className={style.list}>
+                <li className={style.list_item}>
+                    <p className={style.paragraph}>Domposer</p>
+                </li>
+                <li className={style.list_item}>
+                    <p className={style.paragraph}>Bay.js</p>
+                </li>
+                <li className={style.list_item}>
+                    <p className={style.paragraph}>Cookiemunch</p>
+                </li>
+                <li className={style.list_item}>
+                    <p className={style.paragraph}>Screen Time Converter</p>
+                </li>
+                <li className={style.list_item}>
+                    <p className={style.paragraph}>inline.svg</p>
+                </li>
+            </ul>
+        </div>
     )
 }
 
