ce9850b3df444f6e0a03bba86df30a3a70052db8
Trlaic <igortrlajic@gmail.com>
2024-03-18 21:57:32 +0100

    x32: Add hover for social links

--- src/helpers/Arrays.ts
+++ src/helpers/Arrays.ts
@@ -1,10 +1,18 @@
-const headingList = ['Welcome!', 'Grid Dynamics', 'Tyllo', 'Cyberpunk Drive']
+const headingList = ['Welcome!', 'Tyllo', 'Grid Dynamics', 'Cyberpunk Drive']
 
 
 const displayInformationParagraphsArray = [
     {
-        part1: "Hello! I'm Igor Trlaic, a frontend developer based in Belgrade, Serbia, with 2.5 years of professional experience. Beside working in a corporative world on the streaming platform and an e-commerce site, I've also done several projects by myself.",
+        part1: "Hello! I'm Igor Trlaic, a frontend developer based in Belgrade, Serbia, with 2.5 years of professional experience. Beside working in a corporative world on the streaming platform and an e-commerce site, I've also done several projects on my own.",
         part2: "Currently, I'm diving into backend technologies and Three.js because I want to create comprehensive web experiences."
+    },
+    {
+        title: "Software Engineer (specialization UI)",
+        employment: 'Employment period: ',
+        date: "July 2023 - Present",
+        description: "Developing -EON- streaming platform with frontend technologies. Worked in a much smaller team comparing to one in -Grid Dynamics-. In addition to regular job responsibilities which are common for most frontend developers, I've had the opportunity to learn more about streaming technologies and to broaden my knowledge in that field. ",
+        tech: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue', 'Jest', 'Karma', 'Git', 'Jira'],
+        link: "https://eon.tv/"
     },
     {
         title: "Junior Software Engineer (specialization UI)",
@@ -13,14 +21,6 @@ const displayInformationParagraphsArray = [
         description: "Developing -American Eagle- website with HTML, CSS, JavaScript and TypeScript. Collaborated with designers and backend developers for seamless integration and optimal performance. Utilized Ember.js for enhanced user experience and streamlined development. Conducted thorough testing and debugging for cross-browser compatibility. Actively engaged in code reviews and contributed to improving coding standards and best practices within the team.",
         tech: ['HTML', 'SCSS', 'JavaScript', 'TypeScript', 'Ember', 'Jest', 'BitBucket', 'Jira'],
         link: "https://www.ae.com/us/en"
-    },
-    {
-        title: "Software Engineer (specialization UI)",
-        employment: 'Employment period: ',
-        date: "July 2023 - January 2024",
-        description: "Developing -EON- streaming platform with frontend technologies. Worked in a much smaller team comparing to one in -Grid Dynamics-. In addition to regular job responsibilities which are common for most frontend developers, I've had the opportunity to learn more about streaming technologies and to broaden my knowledge in that field. ",
-        tech: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue', 'Jest', 'Karma', 'Git', 'Jira'],
-        link: "https://eon.tv/"
     },
     {
         title: "3D animation for web",
